c27cd19845fea9724f288ffc23c60e1fdbe688d9
Bryan <alex.ari190@gmail.com>
2021-04-22 17:55:59 -0500

    prueba 1

---
+++ fisrt.js
@@ -0,0 +1,25 @@
+const operacion1 = (PA, cantidad)=>{
+    return parseInt(PA) * parseInt(cantidad)
+}
+
+const operacion2 = (hamburgueza, cantidad)=>{
+    return parseInt(hamburgueza) * parseInt(cantidad)
+}
+
+alert("Que Hamburqueza quiere?");
+
+let PA = [20, 25, 28];
+
+let TI = prompt("1: sencilla, 2: dobles o 3: triple");
+
+let N = prompt("Cuantos desea?");
+
+alert("Paga en Efectivo o Tarjeta?");
+
+let pago = prompt("e: Efectivo, t: tarjeta");
+
+if (producto == 1 && e || E){
+    resultado = operacion1(PA[1], cantidad)
+} 
+
+alert('Serian ${resultado}')
