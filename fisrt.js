23aed7aaac85460ba98e3a5169b5a7b9dfd58a96
Bryan <alex.ari190@gmail.com>
2021-04-24 20:00:11 -0500

    update

--- fisrt.js
+++ fisrt.js
@@ -1,25 +1,41 @@
-const operacion1 = (PA, cantidad)=>{
-    return parseInt(PA) * parseInt(cantidad)
+alert("Que hamburgueza desea")
+
+let precio = [20 , 25, 28]
+
+let tipo = prompt("1: sencillas, 2 dobles, 3 triples");
+let cant = +prompt("Cuantas desea");
+let tpago=prompt("Como paga t: tarjeta, e: efectivo")
+
+if (tipo == "1") {
+    if(tpago == "t"){
+        pago = precio[0]*0.05+(cant*precio[0])
+        alert("En total seria $ "+pago +".00")
+    }
+    else if(tpago == "e"){
+        pago = precio[0]*cant
+        alert("En total seria $ "+pago+".00")
+    }   
 }
-
-const operacion2 = (hamburgueza, cantidad)=>{
-    return parseInt(hamburgueza) * parseInt(cantidad)
+else if(tipo == "2"){
+    if(tpago == "t"){
+        pago = precio[1]*0.05+(cant*precio[1])
+        alert("En total seria $ "+pago+".00")
+    }
+    else if(tpago == "e"){
+        pago = precio[1]*cant
+        alert("En total seria $ "+pago+".00")
+    }
 }
-
-alert("Que Hamburqueza quiere?");
-
-let PA = [20, 25, 28];
-
-let TI = prompt("1: sencilla, 2: dobles o 3: triple");
-
-let N = prompt("Cuantos desea?");
-
-alert("Paga en Efectivo o Tarjeta?");
-
-let pago = prompt("e: Efectivo, t: tarjeta");
-
-if (producto == 1 && e || E){
-    resultado = operacion1(PA[1], cantidad)
-} 
-
-alert('Serian ${resultado}')
+else if(tipo == "3"){
+    if(tpago == "t"){
+        pago = precio[2]*0.05+(cant*precio[2])
+        alert("En total seria $ "+pago+".00")
+    }
+    else if(tpago == "e"){
+        pago = precio[2]*cant
+        alert("En total seria $ "+pago+".00")
+    }
+}
+else{
+    alert("ingreso mal refrese y vuelva a pedir su pedido");
+}
